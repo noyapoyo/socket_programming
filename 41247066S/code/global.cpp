a784758295b7cf3d3bc2104c89543c6ef495a38e
scott <scot0227@gmail.com>
2024-12-26 04:11:53 +0800

    修复目录结构

--- 41247066S/code/global.cpp
+++
@@ -1,6 +0,0 @@
-#include "global.h"
-using namespace std;
-map<SSL*, int> clientStatus; // 客户端狀態表
-map <string, SSL*> usernameToSSL;
-pthread_mutex_t usernameToSSLMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t statusMutex = PTHREAD_MUTEX_INITIALIZER;     // 狀態互斥鎖
--- 41247066S/code/sync.sh
+++
@@ -1,1 +0,0 @@
-unison /home/scott0227/test_homeowrk/client_test /home/scott0227/socket_programming/41247066S -force /home/scott0227/socket_programming/41247066S -batch
